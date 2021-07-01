f4ebb82aa4d3219403d29c3f6a5b1b42ff121da4
Michal Matt <michu.em@gmail.com>
2021-07-01 14:53:57 +0200

    adding grup data model, ngFor for list of groups

--- eDziekanat/src/app/groups/groups.component.ts
+++ eDziekanat/src/app/groups/groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Group} from '../shared/group-data.model';
 
 @Component({
   selector: 'app-groups',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups.component.css']
 })
 export class GroupsComponent implements OnInit {
+  public groups: Group[];
 
-  constructor() { }
+  constructor() {
+    this.groups = [
+      new Group('Grupa A', 1),
+      new Group('Grupa B', 2),
+      new Group('Grupa C', 3),
+      new Group('Grupa D', 4),
+      new Group('Grupa E', 5),
+      new Group('Grupa F', 6),
+      new Group('Grupa G', 7)
+    ];
+  }
 
   ngOnInit(): void {
   }
