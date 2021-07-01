194df9df1d43f40a8b2c7c381963a3d691a31c64
Michal Matt <michu.em@gmail.com>
2021-07-01 11:57:54 +0200

    basic components added

--- eDziekanat/src/app/app.module.ts
+++ eDziekanat/src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactItemComponent } from './contact/contact-item/contact-item.component';
+import { ContactFormComponent } from './contact/contact-form/contact-form.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentComponent } from './students/student/student.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { SubjectComponent } from './subjects/subject/subject.component';
+import { GroupsComponent } from './groups/groups.component';
+import { GroupComponent } from './groups/group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ContactItemComponent,
+    ContactFormComponent,
+    StudentsComponent,
+    StudentComponent,
+    SubjectsComponent,
+    SubjectComponent,
+    GroupsComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule
