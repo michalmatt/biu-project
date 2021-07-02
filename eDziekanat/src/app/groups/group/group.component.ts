b6ac685169dd19e8f15c8cd77a53a3fd4b772c86
Michal Matt <michu.em@gmail.com>
2021-07-02 14:59:22 +0200

    adding new stundent to the group

--- eDziekanat/src/app/groups/group/group.component.ts
+++ eDziekanat/src/app/groups/group/group.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {GroupService} from '../../group.service';
+import {Student} from '../../shared/student-data.model';
 
 @Component({
   selector: 'app-group',
@@ -9,8 +11,30 @@ import {ActivatedRoute} from '@angular/router';
 export class GroupComponent implements OnInit {
   public groupId: number;
   public groupName: string;
+  public groupStudents: Student[];
+  public student: Student;
 
-  constructor(private route: ActivatedRoute) { }
+  // tslint:disable-next-line:typedef
+  addToGroup(student: Student) {
+    this.groupService.addToGroup(student);
+    this.groupStudents.push(student);
+    window.alert('Student zostal dodany do grupy');
+  }
+
+  public createStudent(labelText: HTMLInputElement, value: HTMLInputElement, index: HTMLInputElement) {
+    this.student = new Student(labelText.value, value.value, index.valueAsNumber);
+    return this.student;
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private groupService: GroupService,
+  ) {
+    this.groupStudents = [
+      new Student('Terry', 'Black', 16323),
+      new Student('John', 'White', 12823)
+    ];
+  }
 
   ngOnInit(): void {
     this.groupId = this.route.snapshot.params.groupId;
